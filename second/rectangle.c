9cb385e4cfed194c77d95ed158d59cc71c706ef5
Pavi0293 <pavithrastudy10@gmail.com>
2025-08-14 16:07:49 +0530

    Delete second directory

--- second/rectangle.c
+++
@@ -1,5 +0,0 @@
-#include "rectangle.h"
-
-float calculate_area(float length, float width) {
-    return length * width;
-}
--- second/rectangle.h
+++
@@ -1,6 +0,0 @@
-#ifndef RECTANGLE_H
-#define RECTANGLE_H
-
-float calculate_area(float length, float width);
-
-#endif
